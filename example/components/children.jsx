f9420b906712f3e55c2ee5e6408ceaafaaebb4ff
Sergey Gornostaev <svgornostaev@gmail.com>
2018-09-13 23:35:49 +0300

    Add react-highlight (temporarily), edit documentation

--- example/components/children.jsx
+++ example/components/children.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import Highlight from 'react-highlight';
 
 import { withNotifications } from '../../src';
 
@@ -15,7 +16,7 @@ class Children extends Component {
         <header>
           <div className="container">
             <h1>React context notifications</h1>
-            <p>Extremly simple and flexible notifications based on React Context</p>
+            <p>Extremely simple and flexible notifications based on React Context</p>
           </div>
         </header>
         <section>
@@ -29,12 +30,44 @@ class Children extends Component {
             <p>Import notification&apos;s provider and wrap your application in it.</p>
             <p>
               The provider is responsible to pass the data and functions
-              by props down to all a components that will use HOC.
+              by props down to all components that will use HOC.
             </p>
+            <Highlight className="javascript">
+              {`import React from 'react';
+import { render } from 'react-dom';
+import { NotificationsProvider } from 'react-context-notifications';
+
+const App = () => (
+  <NotificationsProvider>
+    <Routes />
+  </NotificationsProvider>
+);
+
+render(<App />, document.getElementById('app'));`}
+            </Highlight>
             <p>
-              You must to import HOC and wrap your component
-              in which you want to manage notifications.
+              To manage notifications you need to import HOC and wrap your component.
+              Component gets list of notifications and functions to manage them.
             </p>
+            <Highlight className="javascript">
+              {`import React from 'react';
+import { withNotifications } from 'react-context-notifications';
+
+const Children = ({ addNotification }) => {
+  const notification = {
+    message: 'Hello!',
+    deleteAfter: 4000,
+  };
+
+  return (
+    <button onClick={() => addNotification(notification)}>
+      Add notification
+    </button>
+  );
+};
+
+export default withNotifications(Children)`}
+            </Highlight>
           </div>
         </section>
       </Fragment>
